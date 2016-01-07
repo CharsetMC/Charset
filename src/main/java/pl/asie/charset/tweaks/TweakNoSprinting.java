c9fe2860df2262f5c2d1a2d760d4499c517b7637
asiekierka <asiekierka@gmail.com>
2016-01-07 19:10:59 +0100

    add noSprinting tweak option

---
+++ src/main/java/pl/asie/charset/tweaks/TweakNoSprinting.java
@@ -0,0 +1,28 @@
+package pl.asie.charset.tweaks;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+public class TweakNoSprinting extends Tweak {
+    public TweakNoSprinting() {
+        super("tweaks", "noPlayerSprinting", "Remove sprinting.", false);
+    }
+
+    @Override
+    public void enable() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Override
+    public void disable() {
+        MinecraftForge.EVENT_BUS.unregister(this);
+    }
+
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.player.isSprinting()) {
+            event.player.setSprinting(false);
+        }
+    }
+}
