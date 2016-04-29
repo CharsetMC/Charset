2c2ac232e25036a9394618695f62ebba938d629c
asiekierka <asiekierka@gmail.com>
2016-04-29 17:20:01 +0200

    Charset 0.3.0-pre1

---
+++ package.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+TMPDIR=$(mktemp -d /tmp/charset.XXXXXXXX)
+OUTDIR=$(pwd)
+mkdir -p releases/"$1"/
+mkdir -p $TMPDIR/in
+
+cd $TMPDIR/in
+unzip "$OUTDIR"/build/libs/Charset-"$1".jar
+mv mcmod.info mcmod-orig.info
+
+for i in `jq -r keys[] "$OUTDIR"/modules.json`; do
+	echo "[" > mcmod.info
+	jq '.[] | select((.modid | ascii_downcase) == "charset'$i'")' mcmod-orig.info >> mcmod.info
+	echo "]" >> mcmod.info
+	if [ "$i" = "lib" ]; then
+		jar cvf "$OUTDIR"/releases/"$1"/charset-"$1"-"$i".jar \
+			assets/charset"$i" \
+			pl/asie/charset/"$i" pl/asie/charset/api \
+			mcmod.info LICENSE*
+	else
+		jar cvf "$OUTDIR"/releases/"$1"/charset-"$1"-"$i".jar \
+			assets/charset"$i" \
+			pl/asie/charset/"$i" \
+			mcmod.info LICENSE*
+	fi
+	rm mcmod.info
+done
+
+cd "$OUTDIR"
+cp modules.json releases/"$1"/
+rm -rf $TMPDIR
