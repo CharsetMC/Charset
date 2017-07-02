de61693adbb876b485e914d5837cf9981de6a0aa
Adrian Siekierka <kontakt@asie.pl>
2017-07-02 14:05:13 +0200

    fix disabling certain modules breaking recipes

---
+++ src/main/java/pl/asie/charset/lib/recipe/ConditionModuleLoadedFactory.java
@@ -0,0 +1,18 @@
+package pl.asie.charset.lib.recipe;
+
+import com.google.gson.JsonObject;
+import net.minecraft.util.JsonUtils;
+import net.minecraftforge.common.crafting.IConditionFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+import pl.asie.charset.ModCharset;
+import pl.asie.charset.lib.loader.ModuleLoader;
+
+import java.util.function.BooleanSupplier;
+
+public class ConditionModuleLoadedFactory implements IConditionFactory {
+    @Override
+    public BooleanSupplier parse(JsonContext context, JsonObject json) {
+        String module = JsonUtils.getString(json, "module");
+        return () -> ModCharset.isModuleLoaded(module);
+    }
+}
