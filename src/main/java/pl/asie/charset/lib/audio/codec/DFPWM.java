5379536e455f3041d783b595e327e907b02e8b14
Adrian Siekierka <kontakt@asie.pl>
2017-09-08 01:35:07 +0200

    relicense to MIT+LGPLv3

--- src/main/java/pl/asie/charset/lib/audio/codec/DFPWM.java
+++ src/main/java/pl/asie/charset/lib/audio/codec/DFPWM.java
@@ -1,7 +1,25 @@
 /*
- * by Ben "GreaseMonkey" Russell, 2013 - Public Domain
+ * Copyright (c) 2015, 2016, 2017 Adrian Siekierka
+ *
+ * This file is part of Charset.
+ *
+ * Charset is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Charset is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Charset.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * by Ben "GreaseMonkey" Russell, 2013 - Public Domain
+ */
 package pl.asie.charset.lib.audio.codec;
 
 /**
