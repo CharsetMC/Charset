85a1da1ab0cff691e4c3bda64cd0aec30f6e60a7
Adrian Siekierka <kontakt@asie.pl>
2018-02-16 11:02:37 +0100

    fix CI compile

--- src/main/java/pl/asie/charset/module/crafting/compression/TileCompressionCrafter.java
+++ src/main/java/pl/asie/charset/module/crafting/compression/TileCompressionCrafter.java
@@ -3,5 +3,4 @@ package pl.asie.charset.module.crafting.compression;
 import pl.asie.charset.lib.block.TileBase;
 
 public class TileCompressionCrafter extends TileBase {
-	private CompressionShape shape;
 }
